0c3bd3e991dcae4f712fad68d08b45b6a6c23528
hejr123 <1090944788@qq.com>
2019-01-26 23:01:08 +0800

    Initial commit

---
+++ design_pattern/src/main/java/com/geely/design/pattern/structural/proxy/staticproxy/Test.java
@@ -0,0 +1,17 @@
+package com.geely.design.pattern.structural.proxy.staticproxy;
+
+import com.geely.design.pattern.structural.proxy.Order;
+
+import java.util.ArrayList;
+
+/**
+ * Created by geely
+ */
+public class Test {
+    public static void main(String[] args) {
+        Order order = new Order();
+        order.setUserId(2);
+        OrderServiceStaticProxy orderServiceStaticProxy = new OrderServiceStaticProxy();
+        orderServiceStaticProxy.saveOrder(order);
+    }
+}
